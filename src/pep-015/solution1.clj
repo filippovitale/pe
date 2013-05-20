7a8d83b5ed2993c56f747200f1534231be286f64
Filippo Vitale <filippo.vitale@gmail.com>
2013-05-20 12:48:43 +1000

    pep-015 solved

---
+++ src/pep-015/solution1.clj
@@ -0,0 +1,10 @@
+(ns pe.pep-015.solution1)
+
+(def solve
+  (memoize
+    (fn [x y]
+      (if (or (zero? x) (zero? y))
+        1
+        (+ (solve (dec x) y) (solve x (dec y)))))))
+
+(solve 20 20)
