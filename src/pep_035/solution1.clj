c33e4a9770853b39d0960698413ac0799983a8a1
Filippo Vitale <filippo.vitale@gmail.com>
2013-05-21 14:03:19 +1000

    pep-035 solved

--- src/pep_035/solution1.clj
+++ src/pep_035/solution1.clj
@@ -1,7 +1,28 @@
 (ns pep-035.solution1)
 
-(defn solve []
-  "TODO")
+;from pep-007
+(defn prime? [n]
+  (.isProbablePrime (BigInteger/valueOf n) 5))
+
+(defn digit-rotation [n]
+  (let [s (str n)
+        l (count s)
+        rotate #(take l (drop % (cycle s)))]
+    (->>
+      (range l)
+      (map rotate)
+      (map #(apply str %))
+      (map #(Integer/parseInt %))
+      )
+    ))
+
+(defn circular-prime? [n]
+  (every? prime? (digit-rotation n)))
+
+(defn solve [n]
+  (count
+    (filter circular-prime?
+      (filter prime? (range 2 n)))))
 
 (defn -main [& _]
-  (solve))
+  (solve 1e6))
--- src/pep_035/wip.clj
+++ src/pep_035/wip.clj
@@ -2,3 +2,50 @@
 
 (defn n->digits [n]
   (map #(Character/getNumericValue %) (str n)))
+
+;from pep-007
+(defn prime? [n]
+  (.isProbablePrime (BigInteger/valueOf n) 5))
+
+(take 10 (cycle [1 2 3]))
+(1 2 3 1 2 3 1 2 3 1)
+
+;197 -> [1 9 7] -> (197 971 719)
+(defn digit-rotation [n]
+  (let [s (str n)
+        l (count s)
+        v (map #(Character/getNumericValue %) s)
+        rotate #(take l (drop % (cycle v)))]
+    (map rotate (range l))))
+
+(defn digit-rotation [n]
+  (let [s (str n)
+        l (count s)
+        v s
+        rotate #(take l (drop % (cycle v)))]
+    (map rotate (range l))))
+
+(map #(Integer/parseInt %) (map #(apply str %) (digit-rotation 112)))
+(112 121 211)
+
+(defn digit-rotation [n]
+  (let [s (str n)
+        l (count s)
+        rotate #(take l (drop % (cycle s)))]
+    (->>
+      (range l)
+      (map rotate)
+      (map #(apply str %))
+      (map #(Integer/parseInt %))
+      )
+    ))
+
+(defn circular-prime? [n]
+  (every? prime? (digit-rotation n)))
+
+(filter circular-prime?
+  (filter prime? (range 2 100)))
+
+(count
+  (filter circular-prime?
+    (filter prime? (range 2 1e6))))
