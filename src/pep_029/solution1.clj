c2da98c1bd514d45fa09e6d0bffb8a1cec5f58eb
Filippo Vitale <filippo.vitale@gmail.com>
2013-08-23 18:54:03 +1000

    pep-029 solved

--- src/pep_029/solution1.clj
+++ src/pep_029/solution1.clj
@@ -1,1 +1,13 @@
 (ns pep-029.solution1)
+
+
+(defn solve [n]
+  (count
+    (set
+      (for [a (range 2 (inc n))
+            b (range 2 (inc n))]
+        (Math/pow a b)
+        ))))
+
+(defn -main [& _]
+  (solve 100))
