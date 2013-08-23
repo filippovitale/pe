6282597d063faee1119fa7f443bded249bac58f8
Filippo Vitale <filippo.vitale@gmail.com>
2013-08-24 00:17:56 +1000

    pep-036 solved

--- src/pep_036/solution1.clj
+++ src/pep_036/solution1.clj
@@ -1,7 +1,20 @@
 (ns pep-036.solution1)
 
+; from pep-004
+(defn palindrome-base10? [n]
+  (let [s (str n)]
+    (= (seq s) (reverse s))))
+
+(defn palindrome-base2? [n]
+  (let [s (Integer/toBinaryString n)]
+    (= (seq s) (reverse s))))
+
 (defn solve [n]
-  nil)
+  (->> n
+    (range)
+    (filter palindrome-base10?)
+    (filter palindrome-base2?)
+    (reduce +)))
 
 (defn -main [& _]
   (solve 1e6))
