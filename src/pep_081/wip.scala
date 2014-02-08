eb09f7736687c7e211a9cfd3afb22ddef216d3a9
Filippo Vitale <fvitale@arbor.net>
2014-02-08 11:16:43 +1100

    started pep 81 in scala

--- src/pep_081/wip.scala
+++ src/pep_081/wip.scala
@@ -1,7 +1,17 @@
 package pep_081
 
 object wip {
-  def main(args: Array[String]) {
-    println("Hello, world!")
+  def main(args: Array[String] = Array("src/pep_081/matrix.txt")) {
+    val solution = solve(args.head)
+    println(solution)
+  }
+
+  def solve(filename: String): Int = {
+    val matrix = io.Source.fromFile(filename)
+      .mkString.trim.split("\n")
+      .map(_.split(",") map (_.toInt))
+
+
+    matrix.last.last // TODO
   }
 }
