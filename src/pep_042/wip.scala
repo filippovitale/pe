6d817b3aebbc7ed7f7ed40d8984ce3a55aff2aba
Filippo Vitale <fvitale@arbor.net>
2014-02-17 12:14:30 +1100

    solved

--- src/pep_042/wip.scala
+++ src/pep_042/wip.scala
@@ -1,13 +1,27 @@
 package pep_042
 
+import scala.collection.immutable._
 
 object wip {
 
   val filename = "src/pep_042/words.txt"
-  val words = io.Source.fromFile(filename).mkString.trim.split(",").map(_.drop(1).dropRight(1))
+  val words = io.Source.fromFile(filename).mkString.trim.split(",").map(_.drop(1).dropRight(1)).toStream
 
-  def solve(): Int = {
-    -1
+  val charToNumber: Map[Char, Int] = ('A' to 'Z').map(c => (c, c.toInt - '@'.toInt)).toMap
+
+  def wordValue(word: String): Int = word.map(charToNumber).sum
+
+  val triangleNumbers: Stream[Int] = {
+    def loop(v: Int): Stream[Int] = {
+      val n = v + 1
+      ((v * n) / 2) #:: loop(n)
+    }
+    loop(1)
+  }
+
+  def solve(words: Seq[String] = words): Int = {
+    val triangleNumbersMap = triangleNumbers.takeWhile(_ <= words.map(wordValue).max).toSet
+    words.map(wordValue).count(triangleNumbersMap.contains)
   }
 
 }
