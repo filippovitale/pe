a74231b23aa8c5ac4e403097983a265231e1eda2
Filippo Vitale <fvitale@arbor.net>
2014-03-26 13:49:14 +1100

    pep_051 solved (but ugly)

--- src/pep_051/Solution.scala
+++ src/pep_051/Solution.scala
@@ -3,16 +3,41 @@ package pep_051
 import scala.collection.immutable.IndexedSeq
 import scala.math.pow
 
-object Solution {
-
-  def solve() = "TODO"
-
-}
-
 object PrimesSeq {
   def apply(minPrime: Int, maxPrime: Int): IndexedSeq[Int] = minPrime until maxPrime filter (isPrime(_))
 
   def apply(digit: Int): IndexedSeq[Int] = apply(pow(10, digit - 1).toInt, pow(10, digit).toInt)
 
   def isPrime(n: Long): Boolean = BigInt(n).isProbablePrime(5)
+}
+
+object Solution {
+
+  val primes6digit = pep_051.PrimesSeq(6).map(_.toString)
+
+  val pattern = for {
+    a <- 0 to 5
+    b <- 0 to 5
+    if b > a
+    c <- 0 to 5
+    if c > b
+    s = (0 to 5).to[Set] - a - b - c
+    x = s.head
+    y = (s - x).head
+    z = (s - x - y).head
+  } yield ((n: String) => (n(a), n(b), n(c)), (n: String) => (n(x), n(y), n(z)))
+
+  def solve() = {
+    val maps = for {
+      p <- pattern
+      aaa <- pep_051.PrimesSeq(6).map(_.toString).groupBy(p._1)
+      bbb = aaa._2
+      ddd = bbb.groupBy(p._2).filter {
+        case (d, _) => d._1 == d._2 && d._2 == d._3
+      }
+      ccc = ddd.size
+    } yield (ddd, ccc)
+
+    maps.filter(_._2 == 8).head._1.values.flatten.min
+  }
 }
--- src/pep_051/Wip.scala
+++ src/pep_051/Wip.scala
@@ -2,8 +2,8 @@ package pep_051
 
 object Wip {
 
-  val primes2digit = PrimesSeq(2).map(_.toString).to[Set]
-  val primes5digit = PrimesSeq(5).map(_.toString).to[Set]
+  //  val primes2digit = PrimesSeq(2).map(_.toString).to[Set]
+  //  val primes5digit = PrimesSeq(5).map(_.toString).to[Set]
 
   //  val maps = for {
   //    i <- 0 to 1
@@ -46,8 +46,7 @@ object Wip {
   //  (1,3,4)
   //  (2,3,4)
 
-  //  val xs = Seq(56003, 56113, 56333, 56443, 56663, 56773, 56993).map(_.toString).to[Set]
-  val xs = Seq(56003, 56013, 56113, 56333, 56443, 56663, 56773, 56993).map(_.toString)
+  val xs = Seq(56003, 56113, 56333, 56443, 56663, 56773, 56993).map(_.toString).to[Set]
 
   (for {
     a <- 0 to 4
@@ -98,9 +97,22 @@ object Wip {
   })
   //  }).head._1
 
-//  def countPrimes(p: String = "56**3", primes: Set[String] = xs): Int = {
-//    1
-//  }
+  //  def countPrimes(p: String = "56**3", primes: Set[String] = xs): Int = {
+  //    1
+  //  }
+
+  val xs2 = Seq(56003, 56013, 56113, 56333, 56443, 56663, 56773, 56993).map(_.toString)
+
+  //  val patterns2 = for {
+  //    a <- 0 to 4
+  //    b <- 0 to 4
+  //    if b > a
+  //    c <- 0 to 4
+  //    if c > b
+  //    s = (0 to 4).to[Set] - a - b - c
+  //    x = s.head
+  //    y = (s - x).head
+  //  } yield ((n: String) => (n(a) + n(b) + n(c)), (n: String) => n(x) + n(y))
 
   val patterns2 = for {
     a <- 0 to 4
@@ -111,19 +123,95 @@ object Wip {
     s = (0 to 4).to[Set] - a - b - c
     x = s.head
     y = (s - x).head
-  } yield ((n: String) => (n(a) + n(b) + n(c)), (n: String) => n(x) + n(y))
+  } yield ((n: String) => (n(a), n(b), n(c)), (n: String) => (n(x), n(y)))
 
-val maps2 = for {
-    p <- patterns2
-  } yield xs.groupBy(p._1).map({
-      case (_, m) => m // TODO filter using p._2 !!!!! same digit!!!! Set(xxxx).size == 1
-    })
-  maps2.flatten
+  //  val maps2 = for {
+  //    p <- patterns2
+  //  } yield xs2.groupBy(p._1).map({
+  //      case (_, m) => m // TODO filter using p._2 !!!!! same digit!!!! Set(xxxx).size == 1
+  //    })
+  //  maps2.flatten
 
-  maps2.map({
-    m => m.values.map(l => (l.head, l.size))
-  }).flatten.filter({
-    case (_, s) => s > 3
-  })
+  //  maps2.map({
+  //    m => m.values.map(l => (l.head, l.size))
+  //  }).flatten.filter({
+  //    case (_, s) => s > 3
+  //  })
+
+  //  val maps2 = for {
+  //    p <- patterns2
+  //    aaa <- xs2.groupBy(p._1)
+  //    bbb = aaa._2
+  //  } yield (bbb, bbb.groupBy(p._2))
+  //  maps2.foreach(println)
+
+  //  val maps2 = for {
+  //    p <- patterns2
+  //    aaa <- xs2.groupBy(p._1)
+  //    bbb = aaa._2
+  //  } yield (bbb.head, bbb.groupBy(p._2))
+  //  maps2.foreach(println)
+
+  // val aaa = Map((4,4) -> List(56443), (1,1) -> List(56113), (0,1) -> List(56013), (0,0) -> List(56003), (9,9) -> List(56993), (6,6) -> List(56663), (3,3) -> List(56333), (7,7) -> List(56773))
+  // aaa.filter({ case (a, b) => a._1 == a._2}).size
+
+  //  val maps2 = for {
+  //    p <- patterns2
+  //    aaa <- xs2.groupBy(p._1)
+  //    bbb = aaa._2
+  //    ccc = bbb.groupBy(p._2).count {
+  //      case (d, _) => d._1 == d._2
+  //    }
+  //  } yield (bbb.head, ccc)
+  //  maps2.filter(_._2 == 7).head
+
+  val patterns3 = for {
+    a <- 0 to 5
+    b <- 0 to 5
+    if b > a
+    c <- 0 to 5
+    if c > b
+    s = (0 to 5).to[Set] - a - b - c
+    x = s.head
+    y = (s - x).head
+    z = (s - x - y).head
+  } yield ((n: String) => (n(a), n(b), n(c)), (n: String) => (n(x), n(y), n(z)))
+
+  val maps3 = for {
+    p <- patterns3
+    aaa <- pep_051.PrimesSeq(6).map(_.toString).groupBy(p._1)
+    bbb = aaa._2
+    ccc = bbb.groupBy(p._2).count {
+      case (d, _) => d._1 == d._2 && d._2 == d._3
+    }
+  } yield (bbb.head, ccc, bbb)
+
+  // 100003 is wrong
+  maps3.filter(_._2 == 8).map(_._1).sorted.head
+
+  // ------------------------------
+
+  val primes6digit = pep_051.PrimesSeq(6).map(_.toString)
+
+  val patterns4 = for {
+    a <- 0 to 4
+    b <- 0 to 4
+    if b > a
+    s = (0 to 4).to[Set] - a - b
+    x = s.head
+    y = (s - x).head
+    z = (s - x - y).head
+  } yield ((n: String) => (n(a), n(b)), (n: String) => (n(x), n(y), n(z)))
+
+  val maps4 = for {
+    p <- patterns4
+    aaa <- primes6digit.groupBy(p._1)
+    bbb = aaa._2
+    ccc = bbb.groupBy(p._2).count {
+      case (d, _) => d._1 == d._2
+    }
+  } yield (bbb.head, ccc)
+
+  maps4.filter(_._2 == 8).map(_._1).sorted.head
 
 }
