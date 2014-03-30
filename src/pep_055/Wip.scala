a70a1489458c925d7141aaecaa21b66e3fd9fe67
Filippo Vitale <fvitale@arbor.net>
2014-03-30 14:11:01 +1100

    pep_055 solved

--- src/pep_055/Wip.scala
+++ src/pep_055/Wip.scala
@@ -1,32 +1,21 @@
 package pep_055
 
-import pep_050.Memo
-import scala.collection.mutable
-
 object Wip {
 
   val MaxIterations = 50
-//  val lychrelNumbers = mutable.Map.empty[Long, Boolean]
-//  def check(x: Long) = lychrelNumbers getOrElseUpdate(x, f(x))
-//  def add(x: Long) = lychrelNumbers getOrElseUpdate(x, f(x))
 
-  def isLychrel(n: Int): Boolean = {
-    var acc: Long = n
-    for (i <- 1 to MaxIterations; if !isPalindrome(acc)) acc += digitReverse(acc)
-    isPalindrome(acc)
-  }
+  def digitReverse(n: BigInt) = BigInt(n.toString().toSeq.reverse.mkString)
 
-  def digitReverse(n: Long) = n.toString.toSeq.reverse.mkString.toInt
-
-  def isPalindrome(n: Long): Boolean = {
-    val ns = n.toString.toSeq
+  def isPalindrome(n: BigInt): Boolean = {
+    val ns = n.toString().toSeq
     ns == ns.reverse
   }
 
-  //  lazy val aaa: Memo[(Long, Int), Boolean] = Memo {
-  //    case (n, MaxIterations) => // isPalindrome(n)
-  //    case (n, i) => // TODO
-  //  }
+  def isLychrel(n: Int): Boolean = {
+    var acc: BigInt = n
+    for (_ <- 1 to MaxIterations; if !isPalindrome(acc)) acc += digitReverse(acc)
+    !isPalindrome(acc)
+  }
 
   def solve(max: Int = 10000) = 0 until max count isLychrel
 
