2ea49924586f1f6ca9e3c79e1ac0396c23f089b7
Filippo Vitale <fvitale@arbor.net>
2014-04-07 22:51:17 +1000

    pep_068 solved

--- src/pep_068/Wip.scala
+++ src/pep_068/Wip.scala
@@ -2,29 +2,38 @@ package pep_068
 
 object Wip {
 
-  val i2c = (i: Int) => ('0'.toInt + i).toChar
+  def describeSet(combinations: Seq[Seq[Int]]): String = combinations.map(_.mkString("")).mkString("")
 
-  def describeSet(combinations: Seq[Seq[Char]]): String = combinations.map(_.mkString("")).mkString("")
-
-  def solve(n: Int = 3) = {
-    def combinations(ext: Vector[Char], int: Vector[Char]): Seq[Seq[Char]] = for {
+  def solve(n: Int = 5) = {
+    def combinations(ext: Vector[Int], int: Vector[Int]): Seq[Seq[Int]] = for {
       i <- 0 until n
       j = (i + 1) % n
     } yield Seq(ext(i), int(i), int(j))
 
     val extInt = for {
-      e0 <- 1 to n * 2
-      e1 <- 1 to n * 2
+      e0 <- 1 to 10
+      e1 <- 1 to 10
       if e0 < e1
-      e2 <- 1 to n * 2
-      if e0 < e2 && e1 != e2
+      e2 <- 1 to 10
+      if e0 < e2 && e2 != e1
+      e3 <- 1 to 10
+      if e0 < e3 && e3 != e1 && e3 != e2
+      e4 <- 1 to 10
+      if e0 < e4 && e4 != e1 && e4 != e2 && e4 != e3
 
-      i0 <- 1 to n * 2
-      if i0 != e0 && i0 != e1 && i0 != e2
-      i1 <- 1 to n * 2
-      if i1 != e0 && i1 != e1 && i1 != e2 && i1 != i0
-      i2 = ((1 to 6).toSet -- Set(e0, e1, e2, i0, i1)).head
-    } yield (Vector(e0, e1, e2) map i2c, Vector(i0, i1, i2) map i2c)
+      i0 <- 1 to 10
+      if i0 != e0 && i0 != e1 && i0 != e2 && i0 != e3 && i0 != e4
+      i1 <- 1 to 10
+      if i1 != e0 && i1 != e1 && i1 != e2 && i1 != e3 && i1 != e4
+      if i1 != i0
+      i2 <- 1 to 10
+      if i2 != e0 && i2 != e1 && i2 != e2 && i2 != e3 && i2 != e4
+      if i2 != i0 && i2 != i1
+      i3 <- 1 to 10
+      if i3 != e0 && i3 != e1 && i3 != e2 && i3 != e3 && i3 != e4
+      if i3 != i0 && i3 != i1 && i3 != i2
+      i4 = ((1 to 10).toSet -- Set(e0, e1, e2, i0, i1, i2, i3)).head
+    } yield (Vector(e0, e1, e2, e3, e4), Vector(i0, i1, i2, i3, i4))
 
     (extInt map {
       case (e, i) => combinations(e, i)
@@ -33,8 +42,7 @@ object Wip {
     } map {
       describeSet
     } filter {
-      _.length == 9
+      _.length == 16 // TODO can we filter the ones != 16 earlier?
     } sorted).last
-    //    }).filter(_.length == 16).sorted.head // TODO can we filter the ones != 16 earlier?
   }
 }
