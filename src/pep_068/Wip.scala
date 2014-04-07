a6375af0f930e8a43e0f9be0b3ead861c79581f0
Filippo Vitale <fvitale@arbor.net>
2014-04-08 09:49:45 +1000

    pep_068 minor optimisation

--- src/pep_068/Wip.scala
+++ src/pep_068/Wip.scala
@@ -11,7 +11,7 @@ object Wip {
     } yield Seq(ext(i), int(i), int(j))
 
     val extInt = for {
-      e0 <- 1 to 10
+      e0 <- 1 to 6
       e1 <- 1 to 10
       if e0 < e1
       e2 <- 1 to 10
@@ -20,19 +20,20 @@ object Wip {
       if e0 < e3 && e3 != e1 && e3 != e2
       e4 <- 1 to 10
       if e0 < e4 && e4 != e1 && e4 != e2 && e4 != e3
+      if Set(e0, e1, e2, e3, e4).contains(10)
 
-      i0 <- 1 to 10
+      i0 <- 1 to 9
       if i0 != e0 && i0 != e1 && i0 != e2 && i0 != e3 && i0 != e4
-      i1 <- 1 to 10
+      i1 <- 1 to 9
       if i1 != e0 && i1 != e1 && i1 != e2 && i1 != e3 && i1 != e4
       if i1 != i0
-      i2 <- 1 to 10
+      i2 <- 1 to 9
       if i2 != e0 && i2 != e1 && i2 != e2 && i2 != e3 && i2 != e4
       if i2 != i0 && i2 != i1
-      i3 <- 1 to 10
+      i3 <- 1 to 9
       if i3 != e0 && i3 != e1 && i3 != e2 && i3 != e3 && i3 != e4
       if i3 != i0 && i3 != i1 && i3 != i2
-      i4 = ((1 to 10).toSet -- Set(e0, e1, e2, i0, i1, i2, i3)).head
+      i4 = ((1 to 9).toSet -- Set(e0, e1, e2, i0, i1, i2, i3)).head
     } yield (Vector(e0, e1, e2, e3, e4), Vector(i0, i1, i2, i3, i4))
 
     (extInt map {
@@ -41,8 +42,6 @@ object Wip {
       _.map(_.sum).distinct.length == 1
     } map {
       describeSet
-    } filter {
-      _.length == 16 // TODO can we filter the ones != 16 earlier?
     } sorted).last
   }
 }
