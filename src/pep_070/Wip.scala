384d47277617e90cdab64e6f5985810fe6916080
Filippo Vitale <fvitale@arbor.net>
2014-04-09 12:29:12 +1000

    pep_070 solved

---
+++ src/pep_070/Wip.scala
@@ -0,0 +1,38 @@
+package pep_070
+
+import EulerPhi.φ
+
+object Wip {
+
+  (6666666 to 10000000).par.map(n => (n, φ(n))).filter{
+    case (n, t) => n.toString.sorted == t.toString.sorted
+  }.seq.sortBy{
+    case (n, t) => n.toDouble / t
+  }.head._1
+
+}
+
+object EulerPhi {
+
+  // http://stackoverflow.com/questions/13216853/calculating-eulers-totient-function-for-very-large-numbers-java
+  def totient(n: Int) = φ(n)
+
+  def φ(n: Int): Int = {
+    var rest = n
+    var result = n
+    for (p: Int <- 2 to math.floor(math.sqrt(n)).toInt) {
+      if (rest % p == 0) {
+        result /= p
+        result *= (p - 1)
+        while (rest % p == 0)
+          rest /= p
+      }
+    }
+    if (rest > 1) {
+      result /= rest
+      result *= (rest - 1)
+    }
+    result
+  }
+
+}
