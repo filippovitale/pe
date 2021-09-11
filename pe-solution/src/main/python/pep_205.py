fa2593ad9bf04e39a316d99ff0dc000070108ea5
Filippo Vitale <fvitale@gmail.com>
2021-09-11 22:06:59 +1000

    housekeeping

--- pe-solution/src/main/python/pep_205.py
+++ pe-solution/src/main/python/pep_205.py
@@ -1,34 +1,18 @@
 from collections import Counter
+from itertools import product
 
-PETER = Counter([a + b + c + d + e + f + g + h + i
-                 for a in [1, 2, 3, 4]
-                 for b in [1, 2, 3, 4]
-                 for c in [1, 2, 3, 4]
-                 for d in [1, 2, 3, 4]
-                 for e in [1, 2, 3, 4]
-                 for f in [1, 2, 3, 4]
-                 for g in [1, 2, 3, 4]
-                 for h in [1, 2, 3, 4]
-                 for i in [1, 2, 3, 4]])
-
-COLIN = Counter([a + b + c + d + e + f
-                 for a in [1, 2, 3, 4, 5, 6]
-                 for b in [1, 2, 3, 4, 5, 6]
-                 for c in [1, 2, 3, 4, 5, 6]
-                 for d in [1, 2, 3, 4, 5, 6]
-                 for e in [1, 2, 3, 4, 5, 6]
-                 for f in [1, 2, 3, 4, 5, 6]])
+PETER = Counter(map(sum, product(range(1, 5), repeat=9)))
+COLIN = Counter(map(sum, product(range(1, 7), repeat=6)))
 
 
 def solve():
-    total_probability = 0
-    for p in PETER.keys():
-        probability = sum([c for n, c in COLIN.items() if p > n])
-        total_probability += probability * PETER.get(p)
+    probability = 0
+    for pn, pc in PETER.items():
+        probability += pc * sum([cc for cn, cc in COLIN.items() if pn > cn])
 
-    total_probability /= sum(PETER.values())
-    total_probability /= sum(COLIN.values())
-    return f"{total_probability:.7f}"
+    probability /= sum(PETER.values())
+    probability /= sum(COLIN.values())
+    return f"{probability:.7f}"
 
 
 if __name__ == "__main__":
