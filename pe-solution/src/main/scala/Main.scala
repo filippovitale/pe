5ed3defe8afb91d343f057f4a2b792e505d9a517
Filippo Vitale <fvitale@gmail.com>
2023-01-02 11:51:52 -0500

    wip 095 in scala

--- pe-solution/src/main/scala/Main.scala
+++ pe-solution/src/main/scala/Main.scala
@@ -1,6 +1,10 @@
 object Main extends App {
   val begin = System.nanoTime()
+<<<<<<< Updated upstream
   val solution = pep_162.Solution.solve()
+=======
+  val solution = pep_095.Wip.Attempt1.solve()
+>>>>>>> Stashed changes
   val ms = (System.nanoTime() - begin) / 1000 / 1000
   println(s"$solution – ${ms}ms")
 }
--- pe-solution/src/main/scala/pep_095/Wip.scala
+++ pe-solution/src/main/scala/pep_095/Wip.scala
@@ -1,5 +1,6 @@
 package pep_095
 
+<<<<<<< Updated upstream
 import common.LongOps
 
 import scala.collection.mutable
@@ -69,6 +70,90 @@ object Wip {
       println(s"(len, n)=($len, $n)")
       n * -1
     }
+=======
+import scalaz.Memo
+
+object Wip {
+
+  val MAX = 1000000
+
+  // TODO move to common - BEGIN
+
+  object IntOps {
+    //from pep-010
+    def isPrime(n: Int): Boolean = BigInt(n).isProbablePrime(5)
+
+    //from pep-021
+    def isPowerOf2(n: Int): Boolean = (n > 0) && (n & (n - 1)) == 0
+
+    val properDivisorsSum: Int => Int = Memo.mutableHashMapMemo {
+      case n if isPrime(n)    => 1
+      case n if isPowerOf2(n) => n - 1
+      case n                  => (1 to n / 2).filter(n % _ == 0).sum
+    }
+
+    def isPerfectNumber(n: Int) = properDivisorsSum(n) == n
+
+    def isAmicable(n: Int) = {
+      val s = properDivisorsSum(n)
+      s != n && properDivisorsSum(s) == n
+    }
+  }
+
+  object LongOps {
+
+    def isPrime(n: Long): Boolean = BigInt(n).isProbablePrime(5)
+
+    def isPowerOf2(n: Long): Boolean = (n > 0) && (n & (n - 1)) == 0
+
+    val properDivisorsSum: Long => Long = Memo.mutableHashMapMemo {
+      case n if isPrime(n)    => 1
+      case n if isPowerOf2(n) => n - 1
+      case n                  => Iterator.from(1).takeWhile(_ <= n / 2).filter(n % _ == 0).sum
+    }
+
+    def isPerfectNumber(n: Long) = properDivisorsSum(n) == n
+
+    def isAmicable(n: Long) = {
+      val s = properDivisorsSum(n)
+      s != n && properDivisorsSum(s) == n
+    }
+  }
+
+  // TODO move to common - END
+
+  /*
+  Interestingly the sum of the proper divisors of 220 is 284 and the sum of the proper divisors of 284 is 220, forming a chain of two numbers.
+  For this reason, 220 and 284 are called an amicable pair.
+
+  Perhaps less well known are longer chains. For example, starting with 12496, we form a chain of five numbers:
+
+  12496 → 14288 → 15472 → 14536 → 14264 (→ 12496 → ...)
+
+  Since this chain returns to its starting point, it is called an amicable chain.
+
+  Find the smallest member of the longest amicable chain with no element exceeding one million.
+   */
+  object Attempt1 {
+
+    type ChainLength = Int
+    type Step = Int
+
+    def aaa(n: Int): Option[ChainLength] = aaa(n, 0)
+
+    def aaa(n: Int, i: Step): Option[ChainLength] = {
+      // when > 1M     : stop [X]
+      // when == start : stop [V] returning the chain length
+      ???
+    }
+
+    val
+
+    // TODO fold peraphs?
+    // TODO monoid peraphs?
+
+    def solve() = (1 to MAX).map(IntOps.properDivisorsSum).head
+>>>>>>> Stashed changes
   }
 
 }
